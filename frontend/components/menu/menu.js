91f68b6f85878e7352379fdc7aa439f2f2e73013
Renan Albuquerque <renan.albuquerque2501@gmail.com>
2025-01-08 17:11:15 -0300

    correção na url dos menus

--- frontend/components/menu/menu.js
+++ frontend/components/menu/menu.js
@@ -11,15 +11,33 @@ export const menu = `
       </span>
     </div>
     <ul>
-      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?cod=${token}&category=politica">Política</a></li>
-      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?cod=${token}&category=economia">Economia</a></li>
-      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?cod=${token}&category=saude">Sáude</a></li>
-      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?cod=${token}&category=culinaria">Culinária</a></li>
-      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?cod=${token}&category=tecnologia">Tecnologia</a></li>
-      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?cod=${token}&category=esporte">Esporte</a></li>
-      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?cod=${token}&category=cultura">Cultura</a></li>
-      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?cod=${token}&category=ciencia">Ciência</a></li>
-      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?cod=${token}&category=educacao">Educação</a></li>
+      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?${
+        token ? "cod=" + token + "&" : ""
+      }category=politica">Política</a></li>
+      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?${
+        token ? "cod=" + token + "&" : ""
+      }category=economia">Economia</a></li>
+      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?${
+        token ? "cod=" + token + "&" : ""
+      }category=saude">Sáude</a></li>
+      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?${
+        token ? "cod=" + token + "&" : ""
+      }category=culinaria">Culinária</a></li>
+      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?${
+        token ? "cod=" + token + "&" : ""
+      }category=tecnologia">Tecnologia</a></li>
+      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?${
+        token ? "cod=" + token + "&" : ""
+      }category=esporte">Esporte</a></li>
+      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?${
+        token ? "cod=" + token + "&" : ""
+      }category=cultura">Cultura</a></li>
+      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?${
+        token ? "cod=" + token + "&" : ""
+      }category=ciencia">Ciência</a></li>
+      <li><a href="https://nosso-brasil.onrender.com/pages/navegar/index.html?${
+        token ? "cod=" + token + "&" : ""
+      }category=educacao">Educação</a></li>
     </ul>
   </nav>
 `;
